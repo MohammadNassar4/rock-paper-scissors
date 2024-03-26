4cc33dd0d99e203d644844d9b5435d86d7c761f0
MohammadNassar4 <138707772+MohammadNassar4@users.noreply.github.com>
2024-03-26 22:17:08 +0200

    Created play round function that returns the round winner

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+function playRound(player, computer) {
+    player.toLowerCase();
+    if (player === computer) {
+        return "it's a tie";
+    }else if ((player === 'rock') && (computer === 'scissors') ||
+    (player === 'paper') && (computer === 'rock') ||
+    (player === 'scissors') && (computer === 'paper')) {
+        return "you win!";
+    }else {
+        return "you lose!";
+    }
+}
